7c120a83c7f3c729d03d500996d11f33dc8c3980
thiyangt <thiyanga.talagala@monash.edu>
2020-10-14 20:09:36 +0530

    updated package with slider function

--- R/find_dataleaks.R
+++ R/find_dataleaks.R
@@ -1,25 +1,33 @@
 #' Correlation calculation based on rolling window with overlapping observations.
 #'
-#' @param x list of time series
+#' @param lstx list of time series
 #' @param h length of forecast horizon
 #' @param cutoff benchmark value for corr, default 1
-#' @importFrom
+#' @importFrom  utils tail
+#' @importFrom plyr ldply
+#' @importFrom purrr map
 #' @return list of matching quantities
 #' @export
-find_dataleaks <- function(x, h, cutoff=1){
-  n <- length(x)
+find_dataleaks <- function(lstx, h, cutoff=1){
+  n <- length(lstx)
   result <- list()
 
   for (i in 1:n){
 
-    y = tail(lst[[i]], h)
-    result[[i]] <- map(lst, ts.match, y=y)
+    y = utils::tail(lstx[[i]], h)
+    result[[i]] <- purrr::map(lstx, ts.match, y=y)
 
   }
 
-  result
 
+  result.list <- purrr::map(result, plyr::ldply, data.frame)
+  n.result.list <- length(result.list)
+  resul.list.clean <- list()
+  for(i in 1:n){
 
+    resul.list.clean[[i]] <- result.list[[i]][-i, ]
+  }
+  resul.list.clean
 }
 #' @examples
 #' a = rnorm(15)
@@ -28,5 +36,13 @@ find_dataleaks <- function(x, h, cutoff=1){
 #'  b = c(rnorm(10), a[1:5]),
 #'  c = rnorm(10)
 #')
-#'ff <- find_dataleaks(lst, h=5)
-#'map(ff, reduce, bind_rows)
+#'find_dataleaks(lst, h=5)
+#'#' a = rnorm(15)
+#'lst <- list(
+#'  a,
+#'  c(rnorm(10), a[1:5]),
+#'  rnorm(10)
+#')
+#'find_dataleaks(lst, h=5)
+
+
